66613f755c0ff80ca021df570bd284544062ebb9
Elysium <elysium@Elysiums-MacBook-Pro.local>
2023-03-06 20:39:15 +0200

    To add other files

---
+++ GeneratePhoto/IPluginImplementation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ProceedPlugin.Sdk;
+namespace GeneratePhoto
+{
+    public class IPluginImplementation : IPlugin
+    {
+        public string Title => "The best plugin produce by Valentyn Medvediuk";
+
+        public string Description => "This plugin generate photo frome user input";
+
+        public void DoSomething()
+        {
+            Console.WriteLine("{Title}+{Description}");
+        }
+    }
+}
+
